d8afec6615191972bbe2e066ec8d3de8f3680f6c
HYBAS22 <hybas22@gmail.com>
2022-11-09 19:50:22 +0300

    Убрал функции с санкциями, переделал всякие штуки

---
+++ YoutubeDownloader/ViewModels/RootViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
+using Stylet;
+using YoutubeDownloader.Services;
+using YoutubeDownloader.Utils;
+using YoutubeDownloader.ViewModels.Components;
+using YoutubeDownloader.ViewModels.Dialogs;
+using YoutubeDownloader.ViewModels.Framework;
+
+namespace YoutubeDownloader.ViewModels;
+
+public class RootViewModel : Screen
+{
+    private readonly IViewModelFactory _viewModelFactory;
+    private readonly DialogManager _dialogManager;
+    private readonly SettingsService _settingsService;
+    private readonly UpdateService _updateService;
+
+    public SnackbarMessageQueue Notifications { get; } = new(TimeSpan.FromSeconds(5));
+
+    public DashboardViewModel Dashboard { get; }
+
+    public RootViewModel(
+        IViewModelFactory viewModelFactory,
+        DialogManager dialogManager,
+        SettingsService settingsService,
+        UpdateService updateService)
+    {
+        _viewModelFactory = viewModelFactory;
+        _dialogManager = dialogManager;
+        _settingsService = settingsService;
+        _updateService = updateService;
+
+        Dashboard = _viewModelFactory.CreateDashboardViewModel();
+
+        DisplayName = $"{App.Name} v{App.VersionString}";
+    }
+
+    private async Task CheckForUpdatesAsync()
+    {
+        try
+        {
+            var updateVersion = await _updateService.CheckForUpdatesAsync();
+            if (updateVersion is null)
+                return;
+
+            Notifications.Enqueue($"Скачиваю обновление {App.Name} v{updateVersion}...");
+            await _updateService.PrepareUpdateAsync(updateVersion);
+
+            Notifications.Enqueue(
+                "Обновление скачано и будет установлено после выхода",
+                "УСТАНОВИТЬ СЕЙЧАС", () =>
+                {
+                    _updateService.FinalizeUpdate(true);
+                    RequestClose();
+                }
+            );
+        }
+        catch
+        {
+            // Failure to update shouldn't crash the application
+            Notifications.Enqueue("Не удалось обновиться");
+        }
+    }
+
+    public async void OnViewFullyLoaded()
+    {
+        await CheckForUpdatesAsync();
+    }
+
+    protected override void OnViewLoaded()
+    {
+        base.OnViewLoaded();
+
+        _settingsService.Load();
+
+        if (_settingsService.IsDarkModeEnabled)
+        {
+            App.SetDarkTheme();
+        }
+        else
+        {
+            App.SetLightTheme();
+        }
+    }
+
+    protected override void OnClose()
+    {
+        base.OnClose();
+
+        Dashboard.CancelAllDownloads();
+
+        _settingsService.Save();
+        _updateService.FinalizeUpdate(false);
+    }
+}
